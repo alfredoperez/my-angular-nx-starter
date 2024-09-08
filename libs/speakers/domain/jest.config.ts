2821eca3ea78fc7684a749fa73e65ea35c7d2ac7
Alfredo Perez <alfredo.perez.q@gmail.com>
2024-09-08 11:57:54 -0500

    fix: correct paths in jest config and project files
    - Update coverageDirectory path in jest.config.ts
    - Correct jestConfig path in project.json
    - Standardize formatting in jest.config.ts

--- libs/speakers/domain/jest.config.ts
+++ libs/speakers/domain/jest.config.ts
@@ -3,20 +3,20 @@ export default {
   displayName: 'speaker-domain',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../coverage/libs/speaker/domain',
+  coverageDirectory: '../../../coverage/libs/speakers/domain',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
+    'jest-preset-angular/build/serializers/html-comment'
   ]
 };
